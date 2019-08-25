7660c0075bda08ca757d57a6aae3556a8a3a449c
Ming-Chih Lin <ricklin126@gmail.com>
2019-08-25 16:37:08 +0800

    feat: Add scripts/
    1. Add script to listen to shutdown signal (not tested yet)
     Changes to be committed:
    	new file:   scripts/README.md
    	new file:   scripts/check-shutdown-signal.sh

---
+++ scripts/README.md
@@ -0,0 +1,2 @@
+# /scripts
+Put those scripts to facilitate the application like performing various build, install, analysis, etc operations.
---
+++ scripts/check-shutdown-signal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# The script listens to the shutdown signal from the container `server-collector`. To make this works,
+# do the following steps:
+# 1. crontab
+#
+# 2. 
+# Add this script to /etc/init.d/ directory
+# chmod +x /etc/init.d/check-shutdown-signal.sh
+echo "waiting" > /var/run/shutdown_signal
+while sleep 30; do 
+  signal=$(cat /var/run/shutdown_signal)
+  if [ "$signal" == "true" ]; then 
+    echo "done" > /var/run/shutdown_signal
+    shutdown -h now
+  fi
+done
